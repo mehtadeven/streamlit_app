c19ddb1a46a08a0b2d79de58ba199c940af1c685
Mehta Deven <devenmehta2@gmail.com>
2025-07-06 20:21:30 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -10,7 +10,7 @@ file =  st.file_uploader("Upload you csv file" ,type=["csv"])
 if file:
   df = pd.read_csv(file)
   # Now want to take a input from user
-  amount = st.number_input(("Enter the amount"), 1, 0, 1)
+  amount = st.number_input(("Enter the amount"), 1)
   st.write(f"You want to convert {amount} rupees to dollar")
   st.dataframe(df)
   selected_currency = st.selectbox("Select the currency",df["Currency"])
